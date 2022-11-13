0b0d2566a684b175a13320b884e7fe17f921bc51
ZhiLeong <saber_zhileong@hotmail.com>
2022-11-12 22:53:06 -0500

    commit tree 26fc8c4b79e4ed52046df4e7d9e7f42663a22e1f

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){
+    return "something";
+}
+function subtract(){
+    return "something";
+}
+function multiply(){
+    return "something";
+}
+function divide(){
+    return "something";
+}
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+let number = 10;
+function increment(number){
+    return (number += 1);
+}
+function decrement(number){
+    return (number -= 1);
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
